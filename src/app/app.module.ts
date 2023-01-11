d436adb44500ac21baadb1f216eb79161dcac3c0
Mykola Antypenko <mykola.antypenko@pearson.com>
2023-01-11 13:12:11 +0200

    Task 1 Introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ProductComponentComponent } from './components/product-component/product-component.component';
+import { ProductListComponentComponent } from './components/product-list-component/product-list-component.component';
+import { CartListComponent } from './components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ProductComponentComponent,
+    ProductListComponentComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule,
