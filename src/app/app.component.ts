a95b6b3897bf8850afb87b5fb654744364f2bb9d
Mykola Antypenko <antipenkonik@gmail.com>
2023-03-31 20:03:43 +0300

    Task 9 Unit tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,8 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
   ViewChild
 } from '@angular/core';
-import { selectQueryParams, selectRouteData, selectRouteParams, selectUrl } from './core/@ngrx';
-import { merge, tap } from 'rxjs';
-import { Store } from '@ngrx/store';
 import { LoginService } from './login/services/login.service';
 
 @Component({
@@ -16,23 +12,13 @@ import { LoginService } from './login/services/login.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit, AfterViewChecked, OnInit {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
   @ViewChild('appTitle')
   title!: ElementRef<HTMLTitleElement>;
 
   constructor(
-    public loginService: LoginService,
-    private store: Store
+    public loginService: LoginService
   ) {}
-
-  ngOnInit() :void {
-    const url$ = this.store.select(selectUrl);
-    const queryParams$ = this.store.select(selectQueryParams);
-    const routeParams$ = this.store.select(selectRouteParams);
-    const routeData$ = this.store.select(selectRouteData);
-    const source$ = merge(url$, queryParams$, routeParams$, routeData$);
-    source$.pipe(tap(val => console.log(val))).subscribe();
-  }
 
   ngAfterViewInit(): void {
     this.title.nativeElement.innerHTML = 'Buy new phones';
--- src/app/shared/directives/stylize/stylize.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StylizeDirective } from './stylize.directive';
-
-describe('StylizeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StylizeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
